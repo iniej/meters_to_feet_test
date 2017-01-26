5a2e83d538cc29b80aaf9d1eca51f3f51c92d734
Iniebiyo <emai.com>
2017-01-25 19:59:02 -0600

    I have fixes the bug.

--- src/main/java/com/clara/DistanceConverter.java
+++ src/main/java/com/clara/DistanceConverter.java
@@ -23,8 +23,9 @@ public class DistanceConverter {
     public static int metersToFeet(int meters){
         //TODO! This method doesn't work yet. Can you fix it?
         //To convert meters to feet, multiply by 3
+       int meteeToFeet = meters*3;
 
-        return 0;
+        return meteeToFeet;
     }
 
     // I know doubles would be more real world but we are keeping it simple
